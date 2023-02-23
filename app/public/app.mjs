c1023d6799ae5c13d26ba22fb6c644d8c74c7daa
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2023-02-23 07:50:50 +0100

    ...

--- app/public/app.mjs
+++ app/public/app.mjs
@@ -11,17 +11,3 @@ const treeId = "learnplaces";
 const response = await fetch(backendConfig.url + ':' + backendConfig.port + '/' + 'repositoryTree');
 const data = await response.json();
 await api.renderTree(parentElement, treeId, data)
-/*
-const xhr = new XMLHttpRequest();
-xhr.open('GET', backendConfig.url + ':' + backendConfig.port + '/' + 'repositoryTree', true);
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET');
-xhr.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type');
-xhr.onreadystatechange = async function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        const response = JSON.parse(xhr.responseText);
-        await api.renderTree(parentElement, treeId, response)
-    }
-};
-xhr.send();*/
